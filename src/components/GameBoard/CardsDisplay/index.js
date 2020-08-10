ddfb8a990e1538994521879e8836d67e4541e4c8
Nick Ray <nickrray@gmail.com>
2020-08-10 17:44:10 -0500

    :construction: Define fxn to check for a match

--- src/components/GameBoard/CardsDisplay/index.js
+++ src/components/GameBoard/CardsDisplay/index.js
@@ -21,6 +21,45 @@ export const CardsDisplay = () => {
     })()
   }, [])
 
+  // Test 'matching' cards using just the card value not suit
+  // TODO: Change matching logic to take into account the card suit as well
+  const checkForMatch = () => {
+    // If cards inside of 'flippedCards' have matching values
+    let isMatch = false
+    let matchedCard1 = null
+    let matchedIndex1 = null
+    let matchedCard2 = null
+    let matchedIndex2 = null
+
+    flippedCards.forEach((card, i) => {
+      matchedCard1 = card
+      matchedIndex1 = i
+      flippedCards.forEach((card2, i2) => {
+        if (i !== i2 && card.value === card2.value) {
+          isMatch = true
+          matchedCard2 = card2
+          matchedIndex2 = i2
+          // Change the matching cards property 'flipped: false' and 'matched: true'
+          matchedCard1.flipped = false
+          matchedCard2.flipped = false
+          matchedCard1.matched = true
+          matchedCard2.matched = true
+        }
+      })
+    })
+
+    if (isMatch) {
+      // Place 'matching cards' into 'matchedCards'
+      setMatchedCards([...matchedCards, matchedCard1, matchedCard2])
+
+      let newCards = cards.map(card => cards.indexOf(card) === matchedIndex1 ? matchedCard1 : card)
+      newCards = cards.map(card => cards.indexOf(card) === matchedIndex2 ? matchedCard2 : card)
+
+      setCards(newCards)
+    }
+
+  }
+
   const handleCardClick = (e) => {
     const flippedCard = cards[e.target.dataset.index]
 
