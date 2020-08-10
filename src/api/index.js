c44c12ba6c8a7a80d1b9b38195ee13b0c66fe7fe
Nick Ray <nickrray@gmail.com>
2020-08-10 16:35:48 -0500

    :recycle: Change two fxns into just one to return the cards only

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,13 @@
 export default {
   async index() {
-    const res = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    return await res.json()
+    const deckRes = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    const deck = await deckRes.json()
+
+    const cardsRes = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=12`)
+    const cards = await cardsRes.json()
+
+    console.log(cards)
+
+    return cards.cards
   }
 }
