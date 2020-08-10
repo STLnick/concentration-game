66a60d2a7610b8d4f1b7dbd5a51fab8537c71273
Nick Ray <nickrray@gmail.com>
2020-08-10 16:36:33 -0500

    :truck: Move useEffect and useState into CardsDisplay

--- src/components/GameBoard/CardsDisplay/Card/index.js
+++ src/components/GameBoard/CardsDisplay/Card/index.js
@@ -3,13 +3,6 @@ import React, { useEffect, useState } from 'react'
 import api from 'api'
 
 export const Card = () => {
-  const [cards, setCards] = useState([])
-
-  useEffect(() => {
-    (async () => {
-      console.log(await api.index())
-    })()
-  }, [])
 
   return (
     <p>I'm a Card</p>
