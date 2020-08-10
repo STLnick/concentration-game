bc381bb71ea39cbca41515e6c79bcb5e5519ce47
Nick Ray <nickrray@gmail.com>
2020-08-10 16:37:02 -0500

    :sparkles: Use api to set cards state in useEffect

--- src/components/GameBoard/CardsDisplay/index.js
+++ src/components/GameBoard/CardsDisplay/index.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import api from 'api'
 
 import { Card } from './Card'
 
 export const CardsDisplay = () => {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const fetchedCards = await api.index()
+
+      setCards(() => fetchedCards.map(card => {
+        card.flipped = false
+        return card
+      }))
+    })()
+  }, [])
 
   return (
     <div>
