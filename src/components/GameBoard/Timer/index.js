41451dcab6864253f0a6e5bd0c87bcc57603c906
Nick Ray <nickrray@gmail.com>
2020-08-10 20:27:29 -0500

    :construction: Grab time prop and render in component

--- src/components/GameBoard/Timer/index.js
+++ src/components/GameBoard/Timer/index.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types'
 
 import './Timer.css'
 
+export const Timer = ({ time, toggle }) => {
+
   return (
-    <h3>Timer</h3>
+    <h3 className="timer">{time}</h3>
   )
 }
 
 Timer.propTypes = {
+  time: PropTypes.string,
   toggle: PropTypes.bool.isRequired
 }
