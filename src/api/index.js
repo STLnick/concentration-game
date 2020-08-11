7c63c80062d4b9c167edcba6665253bca86bfa1c
Nick Ray <nickrray@gmail.com>
2020-08-11 13:09:36 -0500

    :recycle: Change api call to get whole deck and draw 12

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,13 @@
 export default {
   async index() {
+    // const deckRes = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    // const deck = await deckRes.json()
+
+    // Testing 'All Matched condition'
     const deckRes = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     const deck = await deckRes.json()
 
+    // Changed count=12 to count=4 just to test all matched condition
     const cardsRes = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=12`)
     const cards = await cardsRes.json()
 
