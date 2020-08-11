b9e8683db01df7cc9681d0164a3294ba5804c527
Nick Ray <nickrray@gmail.com>
2020-08-10 19:25:26 -0500

    :lipstick: Add margin to cards and matched div

--- src/components/GameBoard/CardsDisplay/Card/index.js
+++ src/components/GameBoard/CardsDisplay/Card/index.js
@@ -2,15 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const Card = ({ flipped, handler, imgSrc, index, matched, suit, value }) => {
+  const imgStyle = {
+    margin: '0.25rem'
+  }
 
   const divStyle = {
     backgroundColor: '#eee',
     border: '1px solid black',
     borderRadius: '5px',
     display: 'inline-block',
-    height: '139px',
+    height: '138px',
+    margin: '0.25rem',
     textAlign: 'center',
-    width: '100px'
+    width: '99px'
   }
 
   const matchedDiv = <div style={divStyle}></div>
@@ -20,6 +24,7 @@ export const Card = ({ flipped, handler, imgSrc, index, matched, suit, value }) 
     data-index={index}
     onClick={handler}
     src={flipped ? imgSrc : 'https://source.unsplash.com/random/100x139'}
+    style={imgStyle}
     width="100"
   />
 
