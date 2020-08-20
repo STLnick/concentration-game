ac3a48c9172ddd075973a07ccb1bef0c8979ac20
Nick Ray <nickrray@gmail.com>
2020-08-20 13:45:24 -0500

    :construction: Start GameOver component

---
+++ client/src/components/GameOver/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { HighScores } from '../HighScores'
+
+export const GameOver = () => {
+
+  return (
+    <div className="container">
+      <h2>Game Over</h2>
+      <HighScores />
+    </div>
+  )
+}
