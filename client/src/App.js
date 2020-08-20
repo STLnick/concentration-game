71a1ef3e283070448629925e5871e42786d57f5b
Nick Ray <nickrray@gmail.com>
2020-08-20 12:48:03 -0500

    :package: Import router items and create basic routes/links

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-import { GameBoard } from './components'
+import { GameBoard, HowToPlay, Welcome } from './components'
 
 import './App.css';
 
 export const App = () => {
   return (
-    <GameBoard />
+    <Router>
+      <Switch>
+        <Route path="/game">
+          <GameBoard />
+        </Route>
+        <Route path="/howtoplay">
+          <HowToPlay />
+        </Route>
+        <Route exact path="/">
+          <Welcome />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
