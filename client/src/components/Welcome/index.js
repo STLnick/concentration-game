fa9894c01323f36fc23eb082afaa7cbf87ab876e
Nick Ray <nickrray@gmail.com>
2020-08-20 12:48:21 -0500

    :sparkles: Create Welcome component

---
+++ client/src/components/Welcome/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
+
+import { HighScores } from './HighScores'
+
+export const Welcome = () => {
+
+  const history = useHistory('/howtoplay')
+
+
+  return (
+    <div className="container welcome-container">
+      <h1 className="welcome">Welcome to Concentration!</h1>
+      <div>
+        {/* link to instructions */}
+      </div>
+      <div>
+        <HighScores />
+      </div>
+      <Link className="link" to="/howtoplay">How To Play</Link>
+      <Link className="link" to="/game">Start the Game</Link>
+    </div>
+  )
+}
