1f90e06f07c2bfb24d17eaeabcdc8755afc27e07
Nick Ray <nickrray@gmail.com>
2020-08-21 14:30:13 -0500

    :sparkles: Utilize api to write new score and fetch all scores

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
+import api from 'api'
 import utils from 'utils'
 
 import { GameBoard, GameOver, HowToPlay, Welcome } from './components'
@@ -12,13 +13,16 @@ import { GameBoard, GameOver, HowToPlay, Welcome } from './components'
 import './App.css';
 
 export const App = () => {
-  const [scores, setScores] = useState([
-    { initials: 'NR', time: '0:40' },
-    { initials: 'AK', time: '0:30' },
-    { initials: 'MM', time: '0:35' },
-  ])
+  const [scores, setScores] = useState([])
   const [time, setTime] = useState(0)
   const [timerIsRunning, setTimerIsRunning] = useState(false)
+
+  // Get scores in database
+  useEffect(() => {
+    (async () => {
+      setScores(await api.getScores())
+    })()
+  }, [])
 
   const handleTimerIsRunning = (toggle) => {
     setTimerIsRunning(toggle)
@@ -36,20 +40,23 @@ export const App = () => {
     }
   })
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
     // Get user name
     const name = e.target.querySelector('#user-name').value
 
-    // TODO: Get user time
+    // Create new score object
+    const newScore = { initials: name, time: utils.secondsToMinutesAndSeconds(time) }
 
-    // TODO: Update state with initials and time
+    // Update state with new score
     setScores(prevScores => {
-      return [...prevScores, { initials: name, time: utils.secondsToMinutesAndSeconds(time) }]
+      return [...prevScores, newScore]
     })
 
     document.querySelector('#form-btn').disabled = true;
+
+    const res = await api.addScore(newScore)
   }
 
   return (
