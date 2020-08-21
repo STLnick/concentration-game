7003a24af5bfa6340f977f48729ec9a3b4c1b5af
Nick Ray <nickrray@gmail.com>
2020-08-21 14:25:17 -0500

    :sparkles: Create database interaction functions

---
+++ server/db/index.js
@@ -0,0 +1,17 @@
+import client from './client'
+
+export const getScores = async () => {
+  try {
+    return await client.db('highscores').collection('scores').find().toArray()
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const addScore = async (score) => {
+  try {
+    return await client.db('highscores').collection('scores').insertOne(score)
+  } catch (err) {
+    throw new Error(err)
+  }
+}
