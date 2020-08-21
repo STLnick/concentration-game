9926dc126a15cfd47eea6bf07ad9ef563897ef64
Nick Ray <nickrray@gmail.com>
2020-08-21 10:20:25 -0500

    :construction: Setup base of GameOver component
    Still need logic to get score from previous page and enter info into state to add to table

--- client/src/components/GameOver/index.js
+++ client/src/components/GameOver/index.js
@@ -4,9 +4,26 @@ import { HighScores } from '../HighScores'
 
 export const GameOver = () => {
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // Get user name
+    const name = e.target.querySelector('input').value
+
+    // TODO: Get user time
+
+    // TODO: Update state with initials and time
+
+  }
+
   return (
     <div className="container">
       <h2>Game Over</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="user-name">Enter Your Name</label>
+        <input id="user-name" placeholder="Bobby Boucher" type="text" />
+        <button type="submit">Submit</button>
+      </form>
       <HighScores />
     </div>
   )
