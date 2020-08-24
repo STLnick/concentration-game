c6839970a23b9c21d12456e49f3842e77917fc95
Nick Ray <nickrray@gmail.com>
2020-08-23 22:21:20 -0500

    :wrench: Add eslint config file

---
+++ server/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-unresolved': [2, { ignore: ['db'] }],
+  },
+};
