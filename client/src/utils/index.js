6f67473ad0e2bd74cafa442a166d75456cd1a109
Nick Ray <nickrray@gmail.com>
2020-08-24 11:07:02 -0500

    :sparkles: Create util function to convert a time string to seconds

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,8 @@
 export default {
   secondsToMinutesAndSeconds(time) {
     return new Date(time * 1000).toISOString().substring(14, 19)
+  },
+  minutesAndSecondsToSeconds(timeStr) {
+    return (Number(timeStr.substring(0, 2)) * 60) + Number(timeStr.substring(3))
   }
 }
