70151e10342cf025ebb511267767d26c03e41c5c
Nick Ray <nickrray@gmail.com>
2020-08-28 13:27:16 -0500

    Add test message on server for heroku deploy

--- server/server.js
+++ server/server.js
@@ -8,14 +8,17 @@ dotenv.config();
 
 const app = express();
 
+// testing
+app.get('/', (_, res) => {
+  res.send('<h1>Hello from Express Server!</h1>');
+});
+
 app.use(cors());
 app.use(express.json());
 
 const PORT = 5000;
 
 app.use('/scores', scores);
-
-app.use;
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on Port:${PORT}`);
